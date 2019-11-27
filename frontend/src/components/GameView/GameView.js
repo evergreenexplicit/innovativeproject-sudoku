8692dfe50ee415ca5e88cfbd3a482e87a806181d
szymonglab <glab.bro@gmail.com>
2019-11-27 20:20:56 +0100

    Creted base for mobile dragging and resized the board on medium devices.

--- frontend/src/components/GameView/GameView.js
+++ frontend/src/components/GameView/GameView.js
@@ -1,9 +1,34 @@
 import React from "react";
 import HTML5Backend from "react-dnd-html5-backend";
+// import MultiBackend from 'react-dnd-multi-backend';
 import { DndProvider } from "react-dnd";
 import "./GameView.scss";
 import Board from "../Board/Board";
 import DragPanel from "../Draggable/DragPanel/DragPanel";
+
+import TouchBackend from "react-dnd-touch-backend";
+import MultiBackend, {
+  TouchTransition,
+  Preview
+} from "react-dnd-multi-backend";
+
+const HTML5toTouch = {
+  backends: [
+    {
+      backend: HTML5Backend
+    },
+    {
+      backend: TouchBackend, // Note that you can call your backends with options
+      options: { enableMouseEvents: true },
+      preview: true,
+      transition: TouchTransition
+    }
+  ]
+};
+
+function generatePreview(type, item, style) {
+  return <div style={{ width: "50px", height: "50px" }}>{type}</div>;
+}
 
 export default function GameView() {
   const mockBoard = [
@@ -19,10 +44,11 @@ export default function GameView() {
   ];
 
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={MultiBackend} options={HTML5toTouch}>
       <div className="gamePanel">
         <Board fields={mockBoard} />
         <DragPanel />
+        <Preview generator={generatePreview} />>
       </div>
     </DndProvider>
   );
