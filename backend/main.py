06eb9b9b58705887fd04fa38ce1c47fcad648fab
Paweł Rubin <pawelrubin19@gmail.com>
2019-12-11 21:51:26 +0100

    typo fixed

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
 from fastapi import FastAPI
 
 from src.auth import connect_to_db
@@ -9,9 +10,15 @@ connect_to_db()
 
 app = FastAPI()
 
+
+origins = [
+    "http:localhost",
+    "http:localhost:8080",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -30,3 +37,5 @@ async def sudoku():
     board = SudokuBoard()
     board.make_puzzle()
     return {"sudokuBoard": board.get_board_matrix()}
+
+app.mount("/*", StaticFiles(directory="../frontend/build"), name="static")
