1d660d0863711ba83de3db0603ad2b9f2e191a39
Paweł Rubin <pawelrubin19@gmail.com>
2019-12-17 14:19:31 +0100

    enpoints

--- frontend/src/components/Auth/Login/Login.js
+++ frontend/src/components/Auth/Login/Login.js
@@ -14,7 +14,7 @@ async function handleSubmit(params, setStatus) {
   formData.append("password", params.data.password);
   (async () => {
     try {
-      await ky.post("http://127.0.0.1:8000/login", {
+      await ky.post("http://sudokubr.me:8000/login", {
         body: formData
       });
     } catch (e) {
