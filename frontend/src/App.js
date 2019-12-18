e4b1a16787c0fecf6c41c82877d68e35b1ad3829
Szymon Głąb <43967199+SzymonGlab@users.noreply.github.com>
2019-12-18 21:29:26 +0100

    Added reseting fields value on click in desktop version and countdown component  (#57)
    Added option to reset a field when mouse is over it. Changed font size on desktops.
    
    Fetching sudoku board from server.
    
    Time adding after board complete.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Register from "./components/Auth/Register/Register";
 import Login from "./components/Auth/Login/Login";
 import Menu from "./components/Menu/Menu";
-import GameView from "./components/GameView/GameView";
+import Board from "./components/Board/Board";
 import Settings from "./components/Settings/Settings";
 import ParticlesComponent from "./components/ParticlesComponent/ParticlesComponent";
 import Results from "./components/Results/Results";
@@ -76,8 +76,7 @@ function App() {
               {/*<Route path="/register" component={Register} />*/}
               {/*<Route path="/login" component={Login} />*/}
               <Route path="/menu" component={Menu} />
-              <Route path="/game" component={GameView} />
-              <Route path="/results" component={Results} />
+              <Route path="/game" component={Board} />
               <Route path="/settings" component={Settings} />
               <Route path="*">
                 <Redirect to="menu" />
